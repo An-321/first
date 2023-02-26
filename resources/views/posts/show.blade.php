50ceb56de82026368748a6d69f7eb44eeadc9ea6
An-321 <an1032e.0210@gmail.com>
2023-02-27 03:56:31 +0900

    ブログ投稿詳細の表示

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -2,24 +2,23 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
-        <title>Blog</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Posts</title>
         <!-- Fonts -->
         <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
     </head>
     <body class="antialiased">
-        <h1>Blog Name</h1>
-        <div class='posts'>
-            @foreach ($posts as $post)
-            <div class='post'>
-                <h2 class='title'>
-                    <a href="/posts/{{ $post->id }}">{{ $post->title }}</a>
-                </h2>
+        <h1 class='title'>
+            {{ $post->title }}
+        </h1>
+        <div class='content'>
+            <div class='content_post'>
+                <h3>本文</h3>
                 <p class='body'>{{ $post->body }}</p>
             </div>
-            @endforeach
         </div>
-        <div class='paginate'>
-            {{ $posts->links() }}
+        <div class="footer">
+            <a href="/">戻る</a>
         </div>
     </body>
 </html>
